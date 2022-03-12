8c23a54a31f3a4d6e4c61be5840d5e068160b459
“[Zubair <[zubairbhuian@gmail.com]>
2022-03-12 15:07:40 +0600

    12.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:form_app/formScreen.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var value = 1;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const FormScreen(),
+      home: SafeArea(
+          child: Scaffold(
+        appBar: AppBar(title: const Text(" My App")),
+        body: Center(
+            child: ElevatedButton(
+                child: Text("Click $value Times"),
+                onPressed: () {
+                  setState(() {
+                    value++;
+                  });
+                })),
+      )),
     );
   }
 }
